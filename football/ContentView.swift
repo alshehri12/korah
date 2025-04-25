1b740cc3a6c3e11dc4dd496152d45e1157210748
alshehri12 <xmx1000@gmail.com>
2025-04-25 15:30:17 +0300

    Initial Commit

---
+++ football/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  football
+//
+//  Created by Abdulrahman Alshehri on 27/10/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ football/footballApp.swift
@@ -0,0 +1,17 @@
+//
+//  footballApp.swift
+//  football
+//
+//  Created by Abdulrahman Alshehri on 27/10/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct footballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
